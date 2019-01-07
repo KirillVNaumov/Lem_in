e448afa028749f0a7e48a6abd374ee99382c7dd6
Kirill Naumov <knaumov@e1z2r3p44.42.us.org>
2019-01-07 13:01:24 -0800

    123

--- srcs/reading_input.c
+++ srcs/reading_input.c
@@ -9,6 +9,16 @@ void        read_number_of_ants(t_map *map, char *line)
     ft_printf("%d\n", map->number_of_ants);
 }
 
+int        read_rooms(t_map *map, char *line)
+{
+
+}
+
+int        read_rooms(t_map *map, char *line)
+{
+    
+}
+
 void        reading_input(t_map *map)
 {
     char    *line;
@@ -17,11 +27,17 @@ void        reading_input(t_map *map)
     phase = 0;
     while (get_next_line(0, &line))
     {
+        if (line[0] == '#' && line[1] != '#')
+            continue ;
+        if (phase == 2)
+            error("Unnecessary information");
         if (!map->number_of_ants)
             read_number_of_ants(map, line);
         if (phase == 0)
-            read_rooms(map, &phase, line);
+            phase += read_rooms(map, line);
         if (phase == 1)
-            read_links(map, &phase, line);       
+            phase += read_links(map, line);       
     }
+    if (phase != 2)
+        error("Not enough information");
 }
